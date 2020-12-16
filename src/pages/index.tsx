c6d46d63833cc18fc11e8d0b7c2a4d996b0318e7
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2020-12-16 13:30:52 -0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,20 +40,20 @@ const Home: React.FC = () => (
         </a>
 
         <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           className={styles.card}
         >
           <h3>Deploy &rarr;</h3>
           <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
+            Instantly deploy your Next.js site to a public URL with Vercel.
           </p>
         </a>
       </div>
     </main>
 
     <footer className={styles.footer}>
-      <a href="https://zeit.co" target="_blank" rel="noopener noreferrer">
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+      <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
+        Powered by <img src="/vercel.svg" alt="Vercel Logo" />
       </a>
     </footer>
 
